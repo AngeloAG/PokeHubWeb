74a5df9878b6570c85d76516b43f9884fee1eb3d
AngeloAG <angelo.ag.079@gmail.com>
2023-02-09 11:53:20 -0700

    Added order and filter functions

--- scripts/search.js
+++ scripts/search.js
@@ -1,6 +1,6 @@
 //This script handles the search to the Pokemon TCG API
 
-//This global variable is to handle the pagin of results
+var results_list = [];
 
 function search() {
   showElementByIdAsBlock("searching-container");
@@ -14,7 +14,7 @@ function search() {
 
 async function getCardInfo(searchTerm) {
   let url = "https://api.pokemontcg.io/v2/cards";
-  url += `?q=name:${searchTerm}*&pageSize=24`; //The search is passed as a param in the url
+  url += `?q=name:${searchTerm}*&pageSize=42`; //The search is passed as a param in the url
   let response = await fetch(url);
   if (response.ok) {
     hideElementById("search-not-found-container");
@@ -29,6 +29,7 @@ async function getCardInfo(searchTerm) {
       showElementByIdAsBlock("search-not-found-container");
     } else {
       document.getElementById("search-results").style.display = "flex";
+      results_list = jsonResponse.data;
       populateResults(jsonResponse.data);
     }
   } else {
@@ -42,14 +43,86 @@ async function getCardInfo(searchTerm) {
 function populateResults(results) {
   let searchResultsElement = document.getElementById("search-results");
   results.forEach((result) => {
-    searchResultsElement.innerHTML += `<div class="card-container"><img class="search-card-img" src=${
-      result.images.small
-    } alt=${result.name}><h2 class="search-prices">Market Avg Price $${
-      result.cardmarket.prices.averageSellPrice
-        ? result.cardmarket.prices.averageSellPrice
-        : "Uknown"
-    }</h2></div>`;
+    let cardPrice;
+    try {
+      cardPrice = result.cardmarket.prices.averageSellPrice;
+    } catch {
+      cardPrice = "Unkown";
+    }
+    searchResultsElement.innerHTML += `<div class="card-container"><img class="search-card-img" src=${result.images.small} alt=${result.id}><h2 class="search-prices">Market Avg Price $${cardPrice}</h2></div>`;
   });
+
+  let cardImages = document.getElementsByClassName("search-card-img");
+
+  for (element of cardImages) {
+    element.addEventListener("click", (event) => {
+      focusImage(event.target.alt);
+    });
+  }
+}
+
+function orderByPrice(order) {
+  if (order == "Ascending") {
+    results_list.sort((current, next) => {
+      let currentPrice, nextPrice;
+      try {
+        currentPrice = current.cardmarket.prices.averageSellPrice;
+      } catch {
+        currentPrice = 0;
+      }
+      try {
+        nextPrice = next.cardmarket.prices.averageSellPrice;
+      } catch {
+        nextPrice = 0;
+      }
+      if (currentPrice > nextPrice) {
+        return -1;
+      }
+      if (currentPrice < nextPrice) {
+        return 1;
+      }
+      return 0;
+    });
+  } else {
+    results_list.sort((current, next) => {
+      let currentPrice, nextPrice;
+      try {
+        currentPrice = current.cardmarket.prices.averageSellPrice;
+      } catch {
+        currentPrice = 0;
+      }
+      try {
+        nextPrice = next.cardmarket.prices.averageSellPrice;
+      } catch {
+        nextPrice = 0;
+      }
+
+      if (currentPrice < nextPrice) {
+        return -1;
+      }
+      if (currentPrice > nextPrice) {
+        return 1;
+      }
+      return 0;
+    });
+  }
+  document.getElementById("search-results").innerHTML = "";
+  populateResults(results_list);
+}
+
+function filterByCardType(type) {
+  let filtered_list = [];
+  filtered_list = results_list.filter((card) => {
+    try {
+      if (card.subtypes.includes(type)) {
+        return true;
+      }
+    } catch {
+      return false;
+    }
+  });
+  document.getElementById("search-results").innerHTML = "";
+  populateResults(filtered_list);
 }
 
 function hideElementById(id) {
@@ -59,3 +132,52 @@ function hideElementById(id) {
 function showElementByIdAsBlock(id) {
   document.getElementById(id).style.display = "block";
 }
+
+function focusImage(cardId) {
+  let card = results_list.filter((card) => card.id == cardId)[0];
+  console.log("Card: ", card);
+  let attacksHTML = `<p class="select-card-info select-card-text">Attacks</p>`;
+  try {
+    card.attacks.forEach((attack) => {
+      attacksHTML += `
+              <p class="select-card-info select-card-text">${attack.name}</p>
+              <p class="select-card-info select-card-text">Cost: ${attack.convertedEnergyCost}</p>
+              <p class="select-card-info select-card-text">Damage: ${attack.damage}</p>
+              <p class="select-card-info select-card-text">${attack.text}</p>`;
+    });
+  } catch {
+    attacksHTML = "";
+  }
+
+  let cardPrice;
+  try {
+    cardPrice = card.cardmarket.prices.averageSellPrice;
+  } catch {
+    cardPrice = "Unkown";
+  }
+
+  document.getElementById(
+    "search-results"
+  ).innerHTML = `<div id="select-result-container">
+          <div id="select-card-img-container">
+            <img id="select-card-img" src="${card.images.small}" alt="${card.id}">
+          </div>
+          <div id="select-card-info-container">
+            <h2 class="select-card-info">Card Name: ${card.name}</h3>
+              <p class="select-card-info select-card-text">Supertype: ${card.supertype}</p>
+              <p class="select-card-info select-card-text">Subtypes: ${card.subtypes}</p>
+              ${attacksHTML}
+              <p class="select-card-info select-card-text">Average price: $${cardPrice}</p>
+          </div>
+        </div>`;
+}
+
+document.querySelector("#orderby-price").addEventListener("change", (event) => {
+  orderByPrice(event.target.value);
+});
+
+document
+  .querySelector("#filterby-cardtype")
+  .addEventListener("change", (event) => {
+    filterByCardType(event.target.value);
+  });
